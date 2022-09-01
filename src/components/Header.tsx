3117f04d78c17f0d7471cac76219c20670a8fbf8
valentimcanejo <valentim.canejo@hotmail.com>
2022-09-01 17:06:54 -0300

    feat: finaliza header.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,9 @@
-import { Flex, Text, Input, Icon } from "@chakra-ui/react";
-import { RiSearchLine } from "react-icons/ri";
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from "@chakra-ui/react";
+import {
+  RiSearchLine,
+  RiNotificationLine,
+  RiUserAddLine,
+} from "react-icons/ri";
 
 export function Header() {
   return (
@@ -41,6 +45,33 @@ export function Header() {
           _placeholder={{ color: "gray.400" }}
         />
         <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
+      <Flex align="center" ml="auto">
+        <HStack
+          spacing="8"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>Rômulo Valentim</Text>
+            <Text color="gray.300" fontSize="small">
+              valentim.canejo@hotmail.com
+            </Text>
+          </Box>
+          <Avatar
+            size="md"
+            name="Rômulo Valentim"
+            src="https://github.com/valentimcanejo.png"
+          />
+        </Flex>
       </Flex>
     </Flex>
   );
